bb04b7665f2e7e217447047fca326e6b64aa4a9a
clee010922 <clee01@stuy.edu>
2018-12-13 09:57:24 -0500

    added skeleton code

---
+++ Node.java
@@ -0,0 +1,27 @@
+public class Node {
+
+  Node next, prev;
+  Integer data;
+
+  public Node next() {
+
+  }
+
+  public Node prev() {
+
+  }
+
+  public void setNext(Node other) {
+
+  }
+
+  public void setPrev(Node other) {
+
+  }
+
+  public String toString() {
+
+  }
+  
+
+}
