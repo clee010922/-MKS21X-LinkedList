fbb1f19505add0904961e159f74712fb8d0b311a
clee010922 <clee01@stuy.edu>
2018-12-13 10:13:35 -0500

    started the MyLinkedList class and added some skeletone code

---
+++ MyLinkedList.java
@@ -0,0 +1,42 @@
+public class MyLinkedList{
+
+  Node start;
+  Node end;
+  int length;
+
+  public MyLinkedList(){
+
+  }
+
+  public boolean add(Integer value) {
+
+  }
+
+  public int size() {
+
+  }
+
+  public String toString() {
+
+  }
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
