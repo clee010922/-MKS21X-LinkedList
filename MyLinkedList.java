702950ddd1a61ee50bb38ea6e9f959f1ea2f0411
clee010922 <clee01@stuy.edu>
2018-12-14 09:39:48 -0500

    added base code

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -20,23 +20,37 @@ public class MyLinkedList{
 
   }
 
-  
+  private Node getNthNode() {
 
+  }
 
+  public Integer get(int index) {
 
+  }
 
+  public Integer set(int index, Integer value) {
 
+  }
 
+  public boolean contains(Integer value) {
 
+  }
 
+  public int indexOf(Integer value) {
 
+  }
 
+  public void add(int index, Integer value) {
 
+  }
 
+  public Integer remove(int index) {
 
+  }
 
+  public boolean remove(Integer value) {
 
-
+  }
 
 
 }
